02aa313ba7834c93655ce593078679c3e9f2473c
jds-codes <agsurjyo@gmail.com>
2026-07-14 11:14:45 +0600

    Etc

--- C#1.cs
+++ C#1.cs
@@ -4,5 +4,7 @@ class Program
     static void Main()
     {
         Console.WriteLine("My name is Surjyo");
+        Console.WriteLine("I am a software engineer");
+        Console.WriteLine("I  am a student of AIUB");
     }
 }
